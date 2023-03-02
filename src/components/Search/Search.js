f98b6b426e879665cb61ce648472fcfef8674d95
XudoyberdiyevDilshod <1998xudoyberdiev@gmail.com>
2023-03-02 08:41:48 +0500

    some changes

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { MovieCard } from "../MovieCard/MovieCard";
+import { Link } from "react-router-dom";
 
 export const Search = () => {
   const [search, setSearch] = useState([]);
@@ -33,7 +33,23 @@ export const Search = () => {
       <div className="container mx-auto mb-3 mt-[100px]">
         <div className="flex justify-between flex-wrap mx-5 gap-4 font-bold text-[35px] text-center my-5">
           {search?.map((item) => (
-            <MovieCard key={item.id} obj={item} />
+            <Link to={`/popular/${item.id}`} className="w-[350px] p-4 bg-slate-400 text-white rounded-md">
+              <ul>
+                <li
+                  key={item.id}
+                  className="text-center p-4 bg-slate-400 text-white rounded-md"
+                >
+                  <img
+                    src={"https://image.tmdb.org/t/p/w500/" + item.poster_path}
+                    width={"100%"}
+                    height={180}
+                    alt="Card Img"
+                  />
+                  <h2 className="text-[20px]">{item.title}</h2>
+                  <p className="text-[18px]">{item.overview}</p>
+                </li>
+              </ul>
+            </Link>
           ))}
         </div>
       </div>
